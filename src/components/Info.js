5539b00099ee099bbde92929252dd03adcaa362e
Harsh Srivastav <hsrivastav45@gmail.com>
2023-03-11 00:41:14 +0530

    Fixed the button not directing correctly issue

--- src/components/Info.js
+++ src/components/Info.js
@@ -6,9 +6,9 @@ function Info() {
             <img src={img} alt="" className="info-image"/>
             <h2>Harsh Srivastav</h2>
             <p>Frontend Developer</p>
-            <a href="#root">harsh.sriv</a>
-            <button className="emailBtn"><i className="fa-solid fa-envelope"></i>  Email</button>
-            <button className="linkedinBtn"><i className="fa-brands fa-linkedin"></i>  LinkedIn</button>
+            <a href="#root" className='info-site'>harsh.sriv</a>
+            <a href="http://www.google.com" target="_blank" className="email" rel="noreferrer"><button className="emailBtn"><i className="fa-solid fa-envelope"></i>  Email</button></a>
+            <a href="https://www.linkedin.com/in/harsh-srivastav-784a8b15b/" target="_blank" className="linkedIn" rel="noreferrer"><button className="linkedinBtn"><i className="fa-brands fa-linkedin" type="button"></i>  LinkedIn</button></a>
         </div>
     )
 }
