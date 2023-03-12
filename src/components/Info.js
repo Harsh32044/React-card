ed9128b9a0be3dd9df2a99dd9fcf1bb139753ef8
Harsh Srivastav <hsrivastav45@gmail.com>
2023-03-13 00:24:14 +0530

    Font Changes

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,7 +7,7 @@ function Info() {
             <h2>Harsh Srivastav</h2>
             <p>Frontend Developer</p>
             <a href="#root" className='info-site'>harsh.sriv</a>
-            <a href="http://www.google.com" target="_blank" className="email" rel="noreferrer"><button className="emailBtn"><i className="fa-solid fa-envelope"></i>  Email</button></a>
+            <button className="emailBtn"><i className="fa-solid fa-envelope"></i>  Email</button>
             <a href="https://www.linkedin.com/in/harsh-srivastav-784a8b15b/" target="_blank" className="linkedIn" rel="noreferrer"><button className="linkedinBtn"><i className="fa-brands fa-linkedin" type="button"></i>  LinkedIn</button></a>
         </div>
     )
