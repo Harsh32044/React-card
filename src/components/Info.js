c051cc5df3de76b9f4aee2242ee71c6a11e82a1f
Harsh Srivastav <hsrivastav45@gmail.com>
2023-03-13 00:44:10 +0530

    Email Btn Issue final resolved

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,17 @@
 import img from '../IMG-20201024-WA0024.jpg'
 
 function Info() {
-    window.location.href = "mailto:hsrivastav45@gmail.com";
+    function openMail() {
+        window.location.href = "mailto:hsrivastav45@gmail.com";
+        return window.location.href;
+    }
     return (
         <div className="info-container">
             <img src={img} alt="" className="info-image"/>
             <h2>Harsh Srivastav</h2>
             <p>Frontend Developer</p>
             <a href="#root" className='info-site'>harsh.sriv</a>
-            <button className="emailBtn" onclick={window.location.href}><i className="fa-solid fa-envelope"></i>  Email</button>
+            <button className="emailBtn" onClick={openMail}><i className="fa-solid fa-envelope"></i>  Email</button>
             <a href="https://www.linkedin.com/in/harsh-srivastav-784a8b15b/" target="_blank" className="linkedIn" rel="noreferrer"><button className="linkedinBtn"><i className="fa-brands fa-linkedin" type="button"></i>  LinkedIn</button></a>
         </div>
     )
